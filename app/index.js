c4303ed68e7ec4ec6a08ff9d337190b99f7f7268
Jason Andersen <jasonandersen00@gmail.com>
2020-05-21 22:32:01 -0600

    Managing State

---
+++ app/index.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import Popular from './components/popular'
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className='container'>
+        <Popular />
+      </div>
+    )
+  }
+}
+
+
+ReactDOM.render(
+  // React Element,
+  <App />,
+  // Where to render the Element to
+  document.getElementById('app')
+)
