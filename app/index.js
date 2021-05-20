2f3ba72ca72f0763093e2178d0a4fc6407137514
Jason Andersen <Jason.Andersen@familysearch.org>
2021-05-20 15:07:40 -0600

    Start over from first example in course

--- app/index.js
+++ app/index.js
@@ -1,23 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+import * as React from 'react'
+import * as ReactDOM from 'react-dom'
 import './index.css'
-import Popular from './components/popular'
-import Battle from './components/Battle'
 
 class App extends React.Component {
   render() {
     return (
-      <div className='container'>
-        <Battle />
+      <div>
+        Hello World!
       </div>
     )
   }
 }
 
-
 ReactDOM.render(
-  // React Element,
   <App />,
-  // Where to render the Element to
   document.getElementById('app')
 )
--- webpack.config.js
+++ webpack.config.js
@@ -9,11 +9,11 @@ module.exports = {
   },
   module: {
     rules: [
-      { test: /\.(js)$/, use: 'babel-loader'},
-      { test: /\.css$/, use: [ 'style-loader', 'css-loader' ] },
+      { test: /\.(js)$/, use: 'babel-loader' },
+      { test: /\.css$/, use: [ 'style-loader', 'css-loader' ]}
     ]
   },
-  mode: process.env.NODE_ENV === 'production' ? 'production' : 'development',
+  mode: 'development',
   plugins: [
     new HtmlWebpackPlugin({
       template: 'app/index.html'
