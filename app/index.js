cd75765bd3919127f5e1a0ec8f16e62a078a6252
Jason Andersen <Jason.Andersen@familysearch.org>
2021-07-27 15:14:51 -0600

    First component done

--- app/index.js
+++ app/index.js
@@ -2,12 +2,16 @@ import * as React from 'react'
 import * as ReactDOM from 'react-dom'
 import './index.css'
 
+
 class App extends React.Component {
   render() {
+
+    const name = 'Jason'
     return (
-      <div>
-        Hello World!
-      </div>
+      <React.Fragment>
+        <h1>👋 Hello {name}!</h1>
+        <p>Today is {new Date().toLocaleString()}</p>
+      </React.Fragment>
     )
   }
 }
