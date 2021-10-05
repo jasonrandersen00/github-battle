d02c4a2f7da1173612ab2d1ed7e7b6f161f5b090
Jason Andersen <Jason.Andersen@familysearch.org>
2021-10-05 13:24:45 -0600

    Handling form state

--- app/index.js
+++ app/index.js
@@ -2,14 +2,14 @@ import * as React from 'react'
 import * as ReactDOM from 'react-dom'
 import './index.css'
 import Popular from './components/Popular'
-
+import Battle from './components/Battle'
 
 class App extends React.Component {
   render() {
     const name = 'Jason'
     return (
       <div className='container'>
-        <Popular />
+        <Battle />
       </div>
     )
   }
