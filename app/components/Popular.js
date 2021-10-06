80038057a4583a2b30155c076ced75583b8116d5
Jason Andersen <Jason.Andersen@familysearch.org>
2021-10-06 14:21:55 -0600

    composition and defaults

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -2,6 +2,8 @@ import * as React from 'react'
 import PropTypes from 'prop-types'
 import {fetchPopularRepos} from '../utils/api'
 import { FaUser, FaStar, FaCodeBranch, FaExclamationTriangle } from 'react-icons/fa'
+import Card from './Card'
+import Loading from './Loading'
 
 function LanguagesNav ({ selected, onUpdateLanguage }) {
   const languages = ['All', 'JavaScript', 'Ruby', 'Java', 'CSS', 'Python']
@@ -36,40 +38,34 @@ function ReposGrid ({repos}) {
         const { login, avatar_url } = owner
 
         return (
-          <li key={html_url} className='card bg-light'>
-            <h4 className='header-lg center-text'>
-              #{index + 1}
-            </h4>
-            <img
-              className='avatar'
-              src={avatar_url}
-              alt={`Avatar for  + ${login}`}
-            />
-            <h2 className='center-text'>
-              <a className='link' href={html_url}>{login}</a>
-            </h2>
-            <ul className='card-list'>
-              <li>
-                <FaUser color='rgb(255, 191, 116)' size={22} />
-                <a href={`https://github.com/${login}`}>
-                  {login}
-                </a>
-              </li>
-              <li>
-                <FaStar color='rgb(255, 215, 0)' size={22} />
-                {stargazers_count.toLocaleString()} stars
-              </li>
-              <li>
-                <FaCodeBranch color='rgb(129, 195, 245)' size={22} />
-                {forks.toLocaleString()} forks
-              </li>
-              <li>
-                <FaExclamationTriangle color='rgb(241, 138, 147)' size={22} />
-                {open_issues.toLocaleString()} open issues
-              </li>
-
-            </ul>
-
+          <li key={html_url}>
+            <Card
+              header={`#${index + 1}`}
+              avatar={avatar_url}
+              href={html_url}
+              name={login}
+            >
+              <ul className='card-list'>
+                <li>
+                  <FaUser color='rgb(255, 191, 116)' size={22} />
+                  <a href={`https://github.com/${login}`}>
+                    {login}
+                  </a>
+                </li>
+                <li>
+                  <FaStar color='rgb(255, 215, 0)' size={22} />
+                  {stargazers_count.toLocaleString()} stars
+                </li>
+                <li>
+                  <FaCodeBranch color='rgb(129, 195, 245)' size={22} />
+                  {forks.toLocaleString()} forks
+                </li>
+                <li>
+                  <FaExclamationTriangle color='rgb(241, 138, 147)' size={22} />
+                  {open_issues.toLocaleString()} open issues
+                </li>
+              </ul>
+            </Card>
           </li>
         )
       })}
@@ -142,7 +138,7 @@ export default class Popular extends React.Component {
           onUpdateLanguage = {this.updateLanguage}
         />
 
-        {this.isLoading() && <p>LOADING</p>}
+        {this.isLoading() && <Loading text='Fetching Repos'/>}
 
         {error && <p className='center-text error'>{error}</p>}
 
