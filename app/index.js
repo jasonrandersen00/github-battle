c14b2d291177d37da8efaf79867a8cca0e77369f
Jason Andersen <Jason.Andersen@familysearch.org>
2021-10-19 16:12:36 -0600

    refactor using class fields

--- app/index.js
+++ app/index.js
@@ -13,15 +13,12 @@ import {
 } from 'react-router-dom'
 
 class App extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      theme: 'light',
-      toggleTheme: () => {
-        this.setState(({ theme }) => ({
-          theme: theme === 'light' ? 'dark' : 'light'
-        }))
-      }
+  state = {
+    theme: 'light',
+    toggleTheme: () => {
+      this.setState(({ theme }) => ({
+        theme: theme === 'light' ? 'dark' : 'light'
+      }))
     }
   }
   render() {
